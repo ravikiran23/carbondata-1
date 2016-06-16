9f02fa3047e87a12d46f9e6761838f475d89de12
ravikiran <ravikiran.sn042@gmail.com>
2016-06-16 18:19:10 +0530

    fixing the review comments.

--- processing/src/main/java/org/carbondata/processing/store/CarbonFactDataHandlerColumnar.java
+++ processing/src/main/java/org/carbondata/processing/store/CarbonFactDataHandlerColumnar.java
@@ -349,6 +349,9 @@ public class CarbonFactDataHandlerColumnar implements CarbonFactHandler {
             .getProperty(CarbonCommonConstants.NUM_CORES_COMPACTING,
                 CarbonCommonConstants.NUM_CORES_DEFAULT_VAL));
       } catch (NumberFormatException exc) {
+        LOGGER.error("Configured value for property " + CarbonCommonConstants.NUM_CORES_COMPACTING
+            + "is wrong.Falling back to the default value "
+            + CarbonCommonConstants.NUM_CORES_DEFAULT_VAL);
         numberOfCores = Integer.parseInt(CarbonCommonConstants.NUM_CORES_DEFAULT_VAL);
       }
     } else {
@@ -357,6 +360,9 @@ public class CarbonFactDataHandlerColumnar implements CarbonFactHandler {
             .getProperty(CarbonCommonConstants.NUM_CORES_LOADING,
                 CarbonCommonConstants.NUM_CORES_DEFAULT_VAL));
       } catch (NumberFormatException exc) {
+        LOGGER.error("Configured value for property " + CarbonCommonConstants.NUM_CORES_LOADING
+            + "is wrong.Falling back to the default value "
+            + CarbonCommonConstants.NUM_CORES_DEFAULT_VAL);
         numberOfCores = Integer.parseInt(CarbonCommonConstants.NUM_CORES_DEFAULT_VAL);
       }
     }
